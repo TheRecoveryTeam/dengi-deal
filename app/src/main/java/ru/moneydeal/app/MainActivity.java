685272b0d6075723a010c8d6365e65cde67f1a2d
Виталий Черков <vitaly.cherkov@gmail.com>
2019-11-24 21:18:35 +0300

    added phone number

--- app/src/main/java/ru/moneydeal/app/MainActivity.java
+++ app/src/main/java/ru/moneydeal/app/MainActivity.java
@@ -4,11 +4,20 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import ru.moneydeal.app.pages.PhoneFragment;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .add(R.id.fragment_container, new PhoneFragment())
+                    .commit();
+        }
     }
 }
